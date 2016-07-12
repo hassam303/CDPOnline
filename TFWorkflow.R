6a0c58dd079ad1c92d39a31143c4f13b8bf8d9bc
Julie <julie.c.chow@gmail.com>
2016-07-12 13:54:57 -0400

    Update TFWorkflow.R

--- TFWorkflow.R
+++ TFWorkflow.R
@@ -1,21 +1,24 @@
 #!/usr/bin/env Rscript
 library(ComplementaryDomainPrioritization)
 
-TFWorkflow <- function(tsvFile, csvFile, startCol, filter = NULL, theta = NULL){
+TFWorkflow <- function(tsvFile, csvFile, startCol, filtering, theta){
   
   tf.pathways = load.WebGestalt(tsvFile, 'TF')
   TFgene.ids = get.genes.tf(tf.pathways)
   
+  startCol = as.numeric(startCol)
+  theta = as.numeric(theta)
+  
   transc.rna = load.gene.data(csvFile,startCol)
   TFprioritized.data = list.filter(transc.rna$transposed.data,TFgene.ids)
   
-  if (is.null(filter)) {
-    next
-  } else if (filter == 1) {
+  if (is.null(filtering) || filtering == "" || is.null(theta)) {
+    return(NULL)
+  } else if (filtering == "m") {
     TFprioritized.50meanfiltered.data = overall.mean.filter(TFprioritized.data, theta)
-  } else if (filter == 2) {
+  } else if (filtering == "v") {
     TFprioritized.50varfiltered.data = overall.var.filter(TFprioritized.data, theta)
-  } else {
+  } else if (filtering == "mv") {
     TFprioritized.50meanfiltered.data = overall.mean.filter(TFprioritized.data, theta)
     TFprioritized.50varfiltered.data = overall.var.filter(TFprioritized.data, theta)
     
@@ -24,7 +27,7 @@ TFWorkflow <- function(tsvFile, csvFile, startCol, filter = NULL, theta = NULL){
 
 args <- commandArgs(trailingOnly = TRUE)
 
-TFWorkflow(args[1], args[2], args[3])
+TFWorkflow(args[1], args[2], args[3], args[4], args[5])
 
 print("Workflow Complete")
 
