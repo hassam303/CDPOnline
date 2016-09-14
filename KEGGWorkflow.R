aaa7c8c4cc83b45eb30b179171c9aa7d0700d772
Hassam Solano <solanomorelhs@g.cofc.edu>
2016-09-14 19:30:24 -0400

    Enabled sending email in KEGGWorkflow

--- KEGGWorkflow.R
+++ KEGGWorkflow.R
@@ -5,6 +5,8 @@ require(sendmailR)
 
 KEGGWorkflow <- function(){
   
+  success <- FALSE #Used to determine if proper files were created
+  
   # Get WebGestalt Data if needed 
   if (length(userData$WG_file_path) == 0){
     system2("irb",
@@ -36,7 +38,14 @@ KEGGWorkflow <- function(){
   
   fixResultsFile(resultFile)
   
-  if (is.null(filtering) || filtering == "" || is.null(theta)) {} 
+  if (is.null(filtering) || filtering == "" || is.null(theta)) {
+   if(length(grep(pattern = "prioritizedData.csv",x = list.dirs(path = paste(getwd(),"/users",userData$jobID,"prioritizedData")),
+                  fixed = TRUE))>0){
+     
+     success <- TRUE
+     
+   } 
+  } 
   else if (filtering == "m") { 
     KEGGprioritized.50meanfiltered.data = overall.mean.filter(KEGGprioritized.data, theta)
     meanResultFile = paste(resultFolder,"/prioritizedData_meanfiltered.csv",sep ="")
@@ -80,6 +89,8 @@ KEGGWorkflow <- function(){
     fixResultsFile(varResultFile)
     fixResultsFile(meanResultFile)
   }
+  
+  sendmail(sucess)
 }
 
 fixResultsFile <- function(filePath){
@@ -89,7 +100,30 @@ fixResultsFile <- function(filePath){
          stdout = NULL)
 }
 
-
+#Start code for SendEmailButton (in Results tab)
+sendEmail <- function(success){
+  if(success){
+    table = read.delim("message.txt", sep = ":")
+    
+    topic = as.character(table$value[[1]])
+    message = as.character(table$value[[2]])
+    
+    sendmail(from = "hassam303@gmail.com", 
+             to = c(userData$email), 
+             subject = topic, 
+             msg = message,
+             control=list(smtpServer="relay.cougars.int"))
+  }
+  else{
+    sendmail(from = "hassam303@gmail.com", 
+             to = c(userData$email), 
+             subject = "CDPOnline:Error Occured", 
+             msg = "something went wrong that caused us not to be able to filter your data #ENTER_ERROR_HERE#, please try again",
+             control=list(smtpServer="relay.cougars.int"))
+    
+  }
+}
+#End code for SendEmailButton (in Results tab)
 
 args <- commandArgs(trailingOnly = TRUE)
 userData <- fromJSON(args[1])
